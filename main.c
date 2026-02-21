ebe01a91be81e76053c7cd3f60843b5e5e364a94
norou66 <norou6@gmail.com>
2026-02-21 17:50:16 +0000

    add: the first stapes of the codextion

--- main.c
+++ main.c
@@ -1,4 +1,13 @@
-int main()
+#include "codexion.h"
+
+int main(int argc, char **argv)
 {
-	printf("Hello, World");
+    t_sim sim;
+    memset(&sim, 0, sizeof(t_sim));
+    if (parse_args(&sim, argc, argv))
+        return (1);
+    printf("Parsed OK: %d coders, burnout=%lld, scheduler=%s\n",
+        sim.num_coders, sim.time_to_burnout,
+        sim.scheduler == SCHED_FIFO ? "fifo" : "edf");
+    return (0);
 }
