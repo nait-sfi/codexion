eecab01737da548d1590a5e1724d3423ced85b44
norou66 <norou6@gmail.com>
2026-02-23 14:42:05 +0000

    add: initialization of simulation,coders, dongeles, with theyre cleanup

---
+++ clean.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clean.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nai-sfi <nait-sfi@student.1337.ma>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/23 14:38:07 by nai-sfi           #+#    #+#             */
+/*   Updated: 2026/02/23 14:38:51 by nai-sfi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "codexion.h"
+
+static void	cleanup_dongles(t_sim *sim)
+{
+	int	i;
+
+	if (!sim->dongles)
+		return ;
+	i = 0;
+	while (i < sim->num_coders)
+	{
+		pthread_mutex_destroy(&sim->dongles[i].mutex);
+		pthread_cond_destroy(&sim->dongles[i].cond);
+		heap_destroy(&sim->dongles[i].queue);
+		i++;
+	}
+	free(sim->dongles);
+	sim->dongles = NULL;
+}
+
+static void	cleanup_coders(t_sim *sim)
+{
+	int	i;
+
+	if (!sim->coders)
+		return ;
+	i = 0;
+	while (i < sim->num_coders)
+	{
+		pthread_mutex_destroy(&sim->coders[i].state_mutex);
+		i++;
+	}
+	free(sim->coders);
+	sim->coders = NULL;
+}
+
+void	cleanup(t_sim *sim)
+{
+	cleanup_dongles(sim);
+	cleanup_coders(sim);
+	pthread_mutex_destroy(&sim->running_mutex);
+	pthread_mutex_destroy(&sim->log_mutex);
+	pthread_mutex_destroy(&sim->counter_mutex);
+}
---
+++ init.c
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nai-sfi <nait-sfi@student.1337.ma>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/22 21:49:31 by nai-sfi           #+#    #+#             */
+/*   Updated: 2026/02/23 14:40:51 by nai-sfi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "codexion.h"
+
+static assign_coder(t_sim * sim, int i)
+{
+sim->coders[i].id = i + 1;
+sim->coders[i].compile_count = 0;
+sim->coders[i].sim = sim;
+sim->coders[i].left_dongle = i;
+sim->coders[i].right_dongle = (i + 1) % sim->num_coders;
+
+}
+
+static int	init_coders(t_sim *sim)
+{
+	int	i;
+
+	sim->coders = malloc(sizeof(t_coder) * sim->num_coders);
+	if (!sim->coders)
+		return (1);
+	i = 0;
+	while (i < sim->num_coders)
+	{
+		memset(&sim->coders[i], 0, sizeof(t_coder));
+		assign_coder(sim, i);
+		if (sim->coders[i].left_dongle <= sim->coders[i].right_dongle)
+		{
+			sim->coders[i].first_dongle = sim->coders[i].left_dongle;
+			sim->coders[i].second_dongle = sim->coders[i].right_dongle;
+		}
+		else
+		{
+			sim->coders[i].first_dongle = sim->coders[i].right_dongle;
+			sim->coders[i].second_dongle = sim->coders[i].left_dongle;
+		}
+		if (pthread_mutex_init(&sim->coders[i].state_mutex, NULL) != 0)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static int	init_dongles(t_sim *sim)
+{
+	int	i;
+
+	sim->dongles = malloc(sizeof(t_dongle) * sim->num_coders);
+	if (!sim->dongles)
+		return (1);
+	i = 0;
+	while (i < sim->num_coders)
+	{
+		memset(&sim->dongles[i], 0, sizeof(t_dongle));
+		sim->dongles[i].id = i;
+		sim->dongles[i].available = 1;
+		sim->dongles[i].release_time = 0;
+		sim->dongles[i].sim = sim;
+		if (pthread_mutex_init(&sim->dongles[i].mutex, NULL) != 0)
+			return (1);
+		if (pthread_cond_init(&sim->dongles[i].cond, NULL) != 0)
+			return (1);
+		heap_init(&sim->dongles[i].queue, sim->num_coders);
+		i++;
+	}
+	return (0);
+}
+
+static int	init_dongles(t_sim *sim)
+{
+	int	i;
+
+	sim->dongles = malloc(sizeof(t_dongle) * sim->num_coders);
+	if (!sim->dongles)
+		return (1);
+	i = 0;
+	while (i < sim->num_coders)
+	{
+		memset(&sim->dongles[i], 0, sizeof(t_dongle));
+		sim->dongles[i].id = i;
+		sim->dongles[i].available = 1;
+		sim->dongles[i].release_time = 0;
+		sim->dongles[i].sim = sim;
+		if (pthread_mutex_init(&sim->dongles[i].mutex, NULL) != 0)
+			return (1);
+		if (pthread_cond_init(&sim->dongles[i].cond, NULL) != 0)
+			return (1);
+		heap_init(&sim->dongles[i].queue, sim->num_coders);
+		i++;
+	}
+	return (0);
+}
+
+int	init_simulation(t_sim *sim)
+{
+	if (pthread_mutex_init(&sim->running_mutex, NULL) != 0)
+		return (1);
+	if (pthread_mutex_init(&sim->log_mutex, NULL) != 0)
+		return (1);
+	if (pthread_mutex_init(&sim->counter_mutex, NULL) != 0)
+		return (1);
+	sim->fifo_counter = 0;
+	sim->running = 0;
+	if (init_coders(sim) != 0)
+		return (1);
+	if (init_dongles(sim) != 0)
+		return (1);
+	return (0);
+}
