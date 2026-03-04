e03159240918182a621a1e8962cca152655892ff
Nouaamane  Ait-sfia <norou6@gmail.com>
2026-03-04 07:03:53 +0000

    fix: prevent memory leak on heap initialization failure by change heap init

--- coders/heap.c
+++ coders/heap.c
@@ -6,23 +6,22 @@
 /*   By: nait-sfi <nait-sfi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/22 15:52:27 by nait-sfi          #+#    #+#             */
-/*   Updated: 2026/03/03 17:44:56 by nait-sfi         ###   ########.fr       */
+/*   Updated: 2026/03/03 17:52:07 by nait-sfi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
-int	heap_init(t_heap *h, int capacity)
+void	heap_init(t_heap *h, int capacity)
 {
 	h->nodes = malloc(sizeof(t_heap_node) * capacity);
 	if (!h->nodes)
 	{
 		fprintf(stderr, "Error: heap malloc failed\n");
-		return (1);
+		return;
 	}
 	h->size = 0;
 	h->capacity = capacity;
-	return (0);
 }
 
 void	heap_destroy(t_heap *h)
